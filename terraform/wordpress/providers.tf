5896b28f6ac3bbca1a9e55cc422e3c8f7797a50f
Rale7 <radovanjevri@gmail.com>
2024-10-26 13:23:44 +0200

    Omogucen wordpress

--- terraform/wordpress/providers.tf
+++ terraform/wordpress/providers.tf
@@ -4,11 +4,11 @@ provider "aws" {
 }
 
 terraform {
-  required_version = "~> 1.5.0"
+  required_version = "~> 1.9.5"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.6.2"
+      version = "~> 5.73.0"
     }
   }
   backend "s3" {
